821f2d10af79dbfca37ecc075f8314c913484320
Indigo <ysu@smail.nju.edu.cn>
2023-10-18 16:32:14 +0800

    get subprocess results

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,35 @@
-use std::io::BufReader;
-use surf;
 use serde_json::{json, Value};
+use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
+use surf;
 use tokio;
 
-async fn send_message(url:&str, title: &str, text: &str) {
+async fn send_message(url: &str, title: &str, text: &str) {
     let client = surf::Client::new();
-    let request = client.post(url).header(
-        "Content-Type",
-        "application/json; charset=utf-8",
-    ).body(json!({
-        "msg_type": "post",
-        "content": {
-            "post": {
-                "zh_cn" : {
-                    "title": title,
-                    "content": [
-                        [
-                            {
-                                "tag": "text",
-                                "text": text
-                            }
+    client
+        .post(url)
+        .header("Content-Type", "application/json; charset=utf-8")
+        .body(json!({
+            "msg_type": "post",
+            "content": {
+                "post": {
+                    "zh_cn" : {
+                        "title": title,
+                        "content": [
+                            [
+                                {
+                                    "tag": "text",
+                                    "text": text
+                                }
+                            ]
                         ]
-                    ]
+                    }
                 }
             }
-        }
-    })).send().await;
+        }))
+        .send()
+        .await
+        .expect("Failed to send message");
 }
 
 #[tokio::main]
@@ -33,7 +37,39 @@ async fn main() {
     let cfg = BufReader::new(std::fs::File::open("config.json").unwrap());
     let json: Value = serde_json::from_reader(cfg).unwrap();
     let url = json["feishu_url"].as_str().unwrap();
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() == 1 {
+        return;
+    }
+    let child = Command::new(&args[1])
+        .args(&args[2..])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn();
+    let command = args[1..].join(" ");
+    let (title, message) = match child {
+        Ok(mut res) => {
+            let mut stdout = String::new();
+            let mut stderr = String::new();
 
-
-    // send_message(url, "Hello", "World").await;
+            let mut lines = BufReader::new(res.stdout.take().unwrap()).lines();
+            while let Some(line) = lines.next() {
+                stdout.push_str(&(line.unwrap() + "\n"));
+            }
+            let mut lines = BufReader::new(res.stderr.take().unwrap()).lines();
+            while let Some(line) = lines.next() {
+                println!("{}", &line.as_ref().unwrap());
+                stderr.push_str(&(line.unwrap() + "\n"));
+            }
+            (
+                format!("Command execution result for: `{}`", command),
+                format!("STDOUT:\n{}\nSTDERR:\n{}", stdout, stderr),
+            )
+        }
+        Err(e) => (
+            format!("Command `{}` failed to execute: ", command),
+            format!("{:?}", e),
+        ),
+    };
+    send_message(url, &title, &message).await;
 }
